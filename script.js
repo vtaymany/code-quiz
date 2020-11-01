5ffa5569719dce66aabf97a3e7e67eaf023ee230
Van Taymany <vtaymany@gmail.com>
2020-10-31 17:01:17 -0700

    adds data structure for the questions

--- script.js
+++ script.js
@@ -16,11 +16,12 @@
 $('#startButton').on('click', function () {
   //hide the welcome message..
   $('.jumbotron').hide()
-
+  //and show the quiz.
+  $('#code-quiz').show()
+  //Start the timer
   var sec = 5
-  var time = setInterval(myTimer, 1000)
-  // and start a timer..
-  function myTimer() {
+  var time = setInterval(timedQuiz, 1000)
+  function timedQuiz() {
     document.getElementById('timeLeft').innerHTML = sec + 's'
     sec--
     //..if the user runs out of time
@@ -29,4 +30,33 @@ $('#startButton').on('click', function () {
       alert('Game Over')
     }
   }
+  var testBank = [
+    {
+      question: 'What is your name?',
+      options: {
+        optionA: 'Mark',
+        optionB: 'Bob',
+        optionC: 'Joe',
+        optionD: 'Yo',
+      },
+      answer: 'A',
+    },
+    {
+      question: 'What is your age?',
+      options: {
+        optionA: '1',
+        optionB: '2',
+        optionC: '3',
+        optionD: '4',
+      },
+      answer: 'B',
+    },
+  ]
+
+  console.log(testBank)
+  //   $('#code-quiz-question').text(testBank.testQuestion.question)
+  //   $('#code-quiz-optionA').text(testBank.testQuestion.options.optionA)
+  //   $('#code-quiz-optionB').text(testBank.testQuestion.options.optionB)
+  //   $('#code-quiz-optionC').text(testBank.testQuestion.options.optionC)
+  //   $('#code-quiz-optionD').text(testBank.testQuestion.options.optionD)
 })
