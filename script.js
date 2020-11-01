c378d0137040b661b7d342c3a5acb1e70d3a770d
Van Taymany <vtaymany@gmail.com>
2020-10-31 18:43:53 -0700

    adds quiz logic

--- script.js
+++ script.js
@@ -13,13 +13,13 @@
 // Coding Quiz
 
 // When the user clicks on the start button..
+var sec = 60
 $('#startButton').on('click', function () {
-  //hide the welcome message..
+  //hides the welcome message..
   $('.jumbotron').hide()
-  //and show the quiz.
+  //and shows the quiz.
   $('#code-quiz').show()
-  //Start the timer
-  var sec = 5
+  //Starts the timer
   var time = setInterval(timedQuiz, 1000)
   function timedQuiz() {
     document.getElementById('timeLeft').innerHTML = sec + 's'
@@ -27,36 +27,56 @@ $('#startButton').on('click', function () {
     //..if the user runs out of time
     if (sec < 0) {
       clearInterval(time)
-      alert('Game Over')
     }
   }
-  var testBank = [
-    {
-      question: 'What is your name?',
-      options: {
-        optionA: 'Mark',
-        optionB: 'Bob',
-        optionC: 'Joe',
-        optionD: 'Yo',
-      },
-      answer: 'A',
-    },
-    {
-      question: 'What is your age?',
-      options: {
-        optionA: '1',
-        optionB: '2',
-        optionC: '3',
-        optionD: '4',
-      },
-      answer: 'B',
-    },
-  ]
+})
 
-  console.log(testBank)
-  //   $('#code-quiz-question').text(testBank.testQuestion.question)
-  //   $('#code-quiz-optionA').text(testBank.testQuestion.options.optionA)
-  //   $('#code-quiz-optionB').text(testBank.testQuestion.options.optionB)
-  //   $('#code-quiz-optionC').text(testBank.testQuestion.options.optionC)
-  //   $('#code-quiz-optionD').text(testBank.testQuestion.options.optionD)
+//Code Quiz
+
+//Test bank
+var questionIndex = 0
+var userScore = 0
+var testBank = [
+  {
+    question: 'What is your name?',
+    options: {
+      optionA: 'Mark',
+      optionB: 'Bob',
+      optionC: 'Joe',
+      optionD: 'Yo',
+    },
+    answer: 'A',
+  },
+  {
+    question: 'What is your age?',
+    options: {
+      optionA: '1',
+      optionB: '2',
+      optionC: '3',
+      optionD: '4',
+    },
+    answer: 'B',
+  },
+]
+
+function printQuestions() {
+  $('#code-quiz-question').text(testBank[questionIndex].question)
+  $('#code-quiz-optionA').text(testBank[questionIndex].options.optionA)
+  $('#code-quiz-optionB').text(testBank[questionIndex].options.optionB)
+  $('#code-quiz-optionC').text(testBank[questionIndex].options.optionC)
+  $('#code-quiz-optionD').text(testBank[questionIndex].options.optionD)
+}
+printQuestions()
+var userInput = $('.code-quiz-options').on('click', function () {
+  userInput = this.value
+  if (userInput == testBank[0].answer) {
+    alert('you got it right')
+    questionIndex++
+    userScore++
+    printQuestions()
+  } else {
+    alert('Try again..but hurry..you are running out of time!!')
+    sec -= 20
+    console.log(sec)
+  }
 })
